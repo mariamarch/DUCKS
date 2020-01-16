ddf588a0324da7b3ac76acdf96d80c0e4d1fbda9
Marie <maria.marchenka@gmail.com>
2020-01-16 13:29:11 +0300

    Созданы все фрагменты и определены знаки для них

--- robot.py
+++ robot.py
@@ -3,8 +3,6 @@ import math
 class Road:
     def _init_(self,x1,y1,seg):
         self.b_cord = x1,y1
-        self.len =1
-        self.wid =2
 
 class Vert_road:
     def _init_(self,cord_b):
@@ -18,6 +16,16 @@ class Vert_road:
             return False
     def dlina(self):
         return self.wid
+    def cord_next(self,sign):
+        self.sign = sign
+        if sign == '+':
+            self.next_x = self.base_x
+            self.next_y = self.base_y + self.wid
+        elif sign == '-':
+            self.next_x = self.base_x
+            self.next_y = self.base_y - self.wid
+        return (self.next_x, self.next_y)
+        return(self.sign)
 
 class Horizontal_road:
     def _init_(self,cord_b):
@@ -31,8 +39,18 @@ class Horizontal_road:
             return False
     def dlina(self):
         return self.len
+    def cord_next(self,sign):
+        self.sign = sign
+        if sign == '+':
+            self.next_x = self.base_x+self.wid
+            self.next_y = self.base_y
+        elif sign == '-':
+            self.next_x = self.base_x-self.wid
+            self.next_y = self.base_y
+        return (self.next_x, self.next_y)
+        return(self.sign)
 
-class sqrt_road_right_down:
+class circle_road_right_down:
     def _init_(self,cord_b):
         self.base_x, self.base_y, self.r1, self.r2 = cord_b
         self.len = math.pi / 2 * (self.r1 + self.r2) / 2
@@ -47,8 +65,13 @@ class sqrt_road_right_down:
             return False
     def dlina(self):
         return self.len
+    def cord_next(self,sign):
+        self.sign='+'
+        self.next_x=self.base_x+self.r1
+        self.next_y=self.base_y+self.r1
+        return(self.next_x,self.next_y,self.sign)
 
-class sqrt_road_right_upper:
+class circle_road_right_upper:
     def _init_(self,cord_b):
         self.base_x, self.base_y, self.r1, self.r2 = cord_b
         self.len = math.pi / 2 * (self.r1 + self.r2) / 2
@@ -57,21 +80,63 @@ class sqrt_road_right_upper:
         ro = math.sqrt(ro)
         sin = (self.base_x-x) / ro
         angle = math.asin(sin)
-        if 0<=angle<=math.pi/2 and self.r1<=ro<=self.r2:
+        if math.pi/2<=angle<=math.pi and self.r1<=ro<=self.r2:
             return True
         else:
             return False
     def dlina(self):
         return self.len
+    def cord_next(self,sign):
+        self.sign = '-'
+        self.next_x = self.base_x + self.r1
+        self.next_y = self.base_y - self.r1
+        return (self.next_x, self.next_y, self.sign)
 
 
-class RoadSector:
-    def _init_(self):
-        pass
-    def is_in_fragment(self, x, y):
-        pass
-    def length(self):
-        pass
+
+class circle_road_left_upper:
+    def _init_(self,cord_b):
+        self.base_x, self.base_y, self.r1, self.r2 = cord_b
+        self.len = math.pi / 2 * (self.r1 + self.r2) / 2
+    def is_in_phragment(self,x,y):
+        ro = ((self.base_x - x) ** 2 + (self.base_y - y) ** 2)
+        ro = math.sqrt(ro)
+        sin = (self.base_x-x) / ro
+        angle = math.asin(sin)
+        if math.pi<=angle<=math.pi*3/2 and self.r1<=ro<=self.r2:
+            return True
+        else:
+            return False
+    def dlina(self):
+        return self.len
+    def cord_next(self,sign):
+        self.sign = '+'
+        self.next_x = self.base_x - self.r1
+        self.next_y = self.base_y - self.r1
+        return (self.next_x, self.next_y,self.sign)
+
+class circle_road_left_down:
+    def _init_(self,cord_b):
+        self.base_x, self.base_y, self.r1, self.r2 = cord_b
+        self.len = math.pi / 2 * (self.r1 + self.r2) / 2
+    def is_in_phragment(self,x,y):
+        ro = ((self.base_x - x) ** 2 + (self.base_y - y) ** 2)
+        ro = math.sqrt(ro)
+        sin = (self.base_x-x) / ro
+        angle = math.asin(sin)
+        if math.pi*3/2<=angle<=math.pi*2 and self.r1<=ro<=self.r2:
+            return True
+        else:
+            return False
+    def dlina(self):
+        return self.len
+    def cord_next(self,sign):
+        self.sign = '+'
+        self.next_x = self.base_x + self.r1
+        self.next_y = self.base_y - self.r1
+        return (self.next_x, self.next_y,self.sign)
+
+
 
 rr1 = Road()
 rr2 = Road()
